15ef9723c928db52410fa983b054131ebc551f85
Jordan Stapinski <jstapins@andrew.cmu.edu>
2017-05-11 23:13:20 -0400

    Added F and C conversions

--- tempConverterFunctions.py
+++ tempConverterFunctions.py
@@ -7,15 +7,22 @@ Python equivalent of 67-272 lab 2: Temperature Converter
 NOTE: python3 -m py_compile <THIS_FILENAME> will compile but not run the script
 '''
 
-def convert(temp):
-	if temp < 474:
-		return "Temperature below Absolute Zero"
+def convert(temp, measure="F"):
 	if type(temp) != int:
 		return "Temperature must be an integer"
-	return 5 * (temp - 32)/9
+	if is_below_absolute_zero(temp, measure):
+		return "Temperature below Absolute Zero"
+	if (measure == "F"):
+		return 5 * (temp - 32)/9
+	if (measure == "C"):
+		return (9 * temp)/5 + 32
 
-print(convert(32))
-print(convert(50))
-print(convert(212))
+def is_below_absolute_zero(temp, measure):
+    return (temp < -454 and measure == "F") or (temp < -270 and measure == "C")
+
+
+print(convert(32, "C"))
+print(convert(50, "C"))
+print(convert(212, "C"))
 print(convert("zero"))
-print(convert(-500))
+print(convert(-500, "C"))
